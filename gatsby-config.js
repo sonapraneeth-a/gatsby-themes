b7289f8dd9993415f03e7e740caffd2e4d13070c
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-09 19:56:45 +0530

    chore(config): updated eslint config for the project
    Added basic rules for eslint and updated path for commitizen binary in
    package.json

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,21 @@ module.exports = {
   __experimentalThemes: [
     {
       resolve: "gatsby-theme-simple-bio",
-      options: {}
-    }
-  ]
+      options: {},
+    },
+  ],
+  plugins: [
+    {
+      resolve: "gatsby-plugin-eslint",
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ["develop"],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
 };
