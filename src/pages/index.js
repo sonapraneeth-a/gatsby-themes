8015df870304c9c7cfa3ed10e5995c6dc9ba12c7
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-24 20:09:29 +0530

    chore(index.js): Added meta component for home page of demo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react";
 
-export default () => <div>Homepage in a user&apos;s site</div>;
+import HeadMetadata from "gatsby-theme-simple-bio/src/components/head/meta";
+
+export default () => (
+  <>
+    <HeadMetadata
+      title="Home page - Demo of gatsby-theme-simple-bio"
+      // eslint-disable-next-line max-len
+      description="This is the home page for the demo of gatsby-theme-simple-bio."
+    />
+    <div>Homepage in a user&apos;s site</div>
+  </>
+);
