ff9c4eb5e973507756246d2e7f553f2b4dcd5770
Sona Praneeth Akula <sonapraneeth.akula@gmail.com>
2019-06-24 20:06:22 +0530

    chore(index.js) Updated home page with head metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react";
 
-export default () => <div>Homepage in a theme&apos;s site</div>;
+import HeadMetadata from "../components/head/meta";
+
+export default () => (
+  <>
+    <HeadMetadata
+      title="Home page - gatsby-theme-simple-bio"
+      // eslint-disable-next-line max-len
+      description="This is the home page of gatsby-theme-simple-bio."
+    />
+    <div>Homepage in a themeer&apos;s site</div>
+  </>
+);
